359f765da6dd7e576955a37c398b3a145ee8ad17
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-04-09 08:39:11 +0530

    operators

---
+++ src/com/operatores/Demo2.java
@@ -0,0 +1,23 @@
+package com.operatores;
+
+public class Demo2 {
+	/**
+	 * 
+	 * Bitwise operator ------------------
+	 * 
+	 * & | ^ >> <<
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		int a = 12;
+		int c = 3;
+		System.out.println(a >> c);   // num*2*n    ->   <<
+		                              // num/2*n    ->    >>
+
+	}
+
+}
