c3d1ac14a3c82dd8b995f415a80d65ad877a25e6
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-04-16 08:36:41 +0530

    Inheritance

---
+++ src/com/inheritancee/X.java
@@ -0,0 +1,10 @@
+package com.inheritancee;
+
+public class X {
+	
+	public int a=34;
+	public void meth1() {
+		System.out.println("this is parent class");
+	}
+
+}
---
+++ src/com/inheritancee/Y.java
@@ -0,0 +1,25 @@
+package com.inheritancee;
+
+public class Y extends X {
+	public void methd2() {
+		System.out.println("this is child class");
+	}
+
+	public static void main(String[] args) {
+//		X x=new Y();
+//		Y y=(Y)new X();
+//		y.meth1();
+//		y.methd2();
+		
+//		X x=new Y();
+//		X x1=(Y)new X();
+//		x1.meth1();
+//		x1.methd2();
+		
+//		X x=new X();
+//		x.meth1();
+//		x.methd2();
+
+	}
+
+}
