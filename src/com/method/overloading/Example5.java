e7843582c37899b8c1241523a951ba165349cb64
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-04-21 16:15:46 +0530

    polymorphism/method oerloading

---
+++ src/com/method/overloading/Example5.java
@@ -0,0 +1,21 @@
+package com.method.overloading;
+
+
+public class Example5 {
+	
+	
+	
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	public static void main(String args) {
+		
+		
+		
+	}
+
+}
